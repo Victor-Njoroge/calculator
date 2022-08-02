6c0d005c9c945c2f34ec45c5726dbcefee0ce4d5
Victor Njoroge <86822599+Victor-Njoroge@users.noreply.github.com>
2022-08-02 20:59:55 +0300

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,13 @@
+function multiplication(){
+    var num1= parseInt(document.getElementById("num1").value);
+    var num2=parseInt(document.getElementById("num2").value);
+  var num3=document.getElementById("num3").value=num1*num2;
+    
+}
+multiplication();
+function division(){
+    var num1 =parseInt(document.getElementById("num1").value);
+    var num2=parseInt(document.getElementById("num2").value);
+    var num3=document.getElementById("num3").value=num1/num2;
+}
+division();
